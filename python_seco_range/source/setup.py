222a446793654028c36e77685a20e04648822f74
btilton-gt <brian.tilton@groundtruth.com>
2020-02-14 14:37:45 -0800

    fix long description content type so pypi renders readme

--- python_seco_range/source/setup.py
+++ python_seco_range/source/setup.py
@@ -1,15 +1,17 @@
-#!/usr/bin/python
-import setuptools
+from setuptools import setup
+from os import path
 
-with open("README.md", 'r') as f:
+readme_path = path.join(path.abspath(path.dirname(__file__)), 'README.md')
+with open(readme_path, encoding='utf-8') as f:
     long_description = f.read()
 
-setuptools.setup(
+setup(
     name='python3-seco-range',
     version = '1.0.0',
     author='ytoolshed',
     description='A Python 3 version of the library to interact with Range from ytoolshed',
     long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/xadrnd/range/tree/master/python_seco_range/source',
     project_urls={
         'Original Project': 'https://github.com/ytoolshed/range'
